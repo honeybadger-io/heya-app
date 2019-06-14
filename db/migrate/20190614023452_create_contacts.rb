2a9af0ae49cc8237cb62bb0d4d02f6b385aeeaa6
Joshua Wood <josh@joshuawood.net>
2019-06-13 19:36:22 -0700

    Add Contact

---
+++ db/migrate/20190614023452_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :email, null: false, index: true
+      t.jsonb :traits, null: false, defualt: {}, index: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_14_023452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email", null: false
+    t.jsonb "traits", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_contacts_on_email"
+    t.index ["traits"], name: "index_contacts_on_traits"
+  end
 
 end

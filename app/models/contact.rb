a017073bd931d48caf4e6eea9c9c634098da1288
Joshua Wood <josh@joshuawood.net>
2019-07-02 21:43:17 -0700

    Add unsubscribe links with Mailkick

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,10 @@
 class Contact < ApplicationRecord
+  include Heya::Concerns::Models::Contact
+
+  mailkick_user
+
+  default_segment { not_opted_out }
+
   store_accessor :traits, :name
 
   def first_name

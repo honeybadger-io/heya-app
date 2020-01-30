10b99252d1116eae7a11ad9efa320ead61fb30b3
Joshua Wood <josh@joshuawood.net>
2020-01-30 09:43:48 -0800

    Add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = {host: "heya.email"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heya.email",
+    address: "smtp.sendgrid.net",
+    port: 465,
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
